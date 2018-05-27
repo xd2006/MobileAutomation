ca7ffdcbec7c6ff4e3fb2b9cc72bab14c6b5b74c
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-05-27 14:04:28 +0300

    Search field test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -17,7 +18,7 @@ public class FirstTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("app.Package", "org.wikipedia");
         capabilities.setCapability("app.Activity", ".main.MainActivity");
@@ -34,6 +35,8 @@ public class FirstTest {
     @Test
     public void firstTest()
     {
-        System.out.println("First test run");
+        WebElement element = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element.click();
+//        System.out.println("First test run");
     }
 }
