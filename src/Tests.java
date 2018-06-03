631d5bc07e1897300e0d2d76d40b0ec2389cb58e
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-06-03 16:06:28 +0300

    saveFirstArticleToMyList - initial test

--- src/Tests.java
+++ src/Tests.java
@@ -226,6 +226,62 @@ public class Tests {
 
     }
 
+    @Test
+    public void saveFirstArticleToMyList(){
+
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                15);
+
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                15);
+
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Search Wikipedia' input",
+                15);
+
+        waitForElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15);
+
+        waitForElementAndClick(By.xpath("//android.widget.ImageView[@content_desc='More options']"),
+                "Cannot find button to open article options", 5);
+
+        waitForElementAndClick(By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find option to add article to reading list", 5);
+
+        waitForElementAndClick(By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay", 5);
+
+        waitForElementAndClear(By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of articles folder", 5);
+
+        waitForElementAndSendKeys(By.id("org.wikipedia:id/text_input"), "Learning programming",
+        "Cannot put text into articles folder input", 5);
+
+        waitForElementAndClick(By.xpath("//*[@text='OK']"),
+                "Cannot press 'OK' button", 5);
+
+        waitForElementAndClick(By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X line", 5);
+
+        waitForElementAndClick(By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find navigation button to 'My lists'", 5);
+
+        waitForElementAndClick(By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find created folder", 5);
+
+        swipeElementToLeft(By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find save article");
+
+        waitForElementNotPresent(By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article", 5);
+
+    }
+
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -286,8 +342,12 @@ public class Tests {
         int end_y = (int) (size.height * 0.2);
 
         TouchAction action = new TouchAction(driver);
-        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
-
+        action.
+                press(x, start_y).
+                waitAction(timeOfSwipe).
+                moveTo(x, end_y).
+                release().
+                perform();
     }
 
     protected void swipeUpQuick(){
@@ -306,6 +366,24 @@ public class Tests {
            swipeUpQuick();
            ++already_swiped;
         }
+    }
+
+    protected void swipeElementToLeft(By by, String error_message ){
+
+        WebElement element = waitForElementPresent(by, error_message, 10);
+
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y) / 2;
+
+        TouchAction action = new TouchAction(driver);
+        action.
+                press(right_x, middle_y).
+                waitAction(150).
+                moveTo(left_x, middle_y).
+                release().perform();
 
     }
 }
