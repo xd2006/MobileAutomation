edac0019d501d637d6ba3573d83f6786f02cf98b
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-06-16 00:17:19 +0300

    Platform selection refactoring

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,13 +1,8 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
 
 public abstract class CoreTestCase extends TestCase {
 
@@ -15,17 +10,15 @@ public abstract class CoreTestCase extends TestCase {
     private static final String PLATFORM_ANDROID = "android";
 
     protected AppiumDriver driver;
-//    private static String AppiumUrl = "http://127.0.0.1:4723/wd/hub";
+    protected Platform platform;
 
     @Override
     protected void setUp() throws Exception {
 
         super.setUp();
 
-        DesiredCapabilities capabilities = getCapabilitiesByPlatformEnv();
-        URL appiumUrl = getAppiumUrlByPlatformEnv();
-
-        driver = getDriverByPlatformEnv(appiumUrl, capabilities);
+        platform = new Platform();
+        driver = platform.getDriver();
     }
 
 
@@ -47,75 +40,5 @@ public abstract class CoreTestCase extends TestCase {
         driver.runAppInBackground(seconds);
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
 
-        String platform = System.getenv("PLATFORM");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "6.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("app.Package", "org.wikipedia");
-            capabilities.setCapability("app.Activity", ".main.MainActivity");
-//        capabilities.setCapability("app", "C:\\Sources\\MobileAutomation\\apks\\org.wikipedia.apk");
-            capabilities.setCapability("app", "C:\\Sources\\Study\\MobileAutomation\\apks\\org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-
-
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone SE");
-            capabilities.setCapability("platformVersion", "11.2");
-            capabilities.setCapability("app", "/Users/alex/Desktop/Wikipedia.app");
-        }
-            else {
-            throw new Exception("Cannot get run platform value from env variable. Platform value " + platform);
-        }
-        return capabilities;
-    }
-
-    private AppiumDriver getDriverByPlatformEnv(URL appiumUrl, DesiredCapabilities capabilities) throws Exception {
-
-        String platform = System.getenv("PLATFORM");
-        AppiumDriver driver;
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-
-            driver = new AndroidDriver(appiumUrl, capabilities);
-        } else if (platform.equals(PLATFORM_IOS))
-        {
-            driver = new IOSDriver(appiumUrl, capabilities);
-        }
-        else{
-
-            throw new Exception("Cannot get run platform value from env variable. Platform value " + platform);
-
-        }
-
-        return driver;
-    }
-
-    private URL getAppiumUrlByPlatformEnv() throws Exception {
-
-        String platform = System.getenv("PLATFORM");
-        String appiumUrl;
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            appiumUrl = "http://EPBYMINW6539.minsk.epam.com:4723/wd/hub";
-
-        } else if (platform.equals(PLATFORM_IOS))
-        {
-            appiumUrl = "http://10.6.113.26:4723/wd/hub";;
-        }
-        else{
-
-            throw new Exception("Cannot get run platform value from env variable. Platform value " + platform);
-
-        }
-
-        return new URL(appiumUrl);
-    }
 }
---
+++ src/lib/Platform.java
@@ -0,0 +1,94 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class Platform {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
+    public AppiumDriver getDriver() throws Exception {
+
+        URL url = getAppiumUrlByPlatformEnv();
+        if (isAndroid()){
+            return new AndroidDriver(url, getAndroidDesiredCapabilities());
+        } else if (isIOS()){
+            return new IOSDriver(url, getIOSDesiredCapabilities());
+        } else {
+            throw new Exception("Cannot detect type of the driver. Platform value: " + getPlatformVar());
+        }
+    }
+
+    public Boolean isAndroid(){
+        return isPlatform(PLATFORM_ANDROID);
+    }
+
+    public Boolean isIOS(){
+        return isPlatform(PLATFORM_IOS);
+    }
+
+
+
+    private DesiredCapabilities getAndroidDesiredCapabilities(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("app.Package", "org.wikipedia");
+        capabilities.setCapability("app.Activity", ".main.MainActivity");
+//        capabilities.setCapability("app", "C:\\Sources\\MobileAutomation\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("app", "C:\\Sources\\Study\\MobileAutomation\\apks\\org.wikipedia.apk");
+
+        return capabilities;
+    }
+
+    private DesiredCapabilities getIOSDesiredCapabilities(){
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone SE");
+        capabilities.setCapability("platformVersion", "11.2");
+        capabilities.setCapability("app", "/Users/alex/Desktop/Wikipedia.app");
+
+        return capabilities;
+    }
+
+    private Boolean isPlatform(String my_platform){
+        String platform = getPlatformVar();
+        return my_platform.equals(platform);
+
+    }
+
+    private String getPlatformVar(){
+        return System.getenv("PLATFORM");
+    }
+
+    private URL getAppiumUrlByPlatformEnv() throws Exception {
+
+        String platform = getPlatformVar();
+        String appiumUrl;
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            appiumUrl = "http://EPBYMINW6539.minsk.epam.com:4723/wd/hub";
+
+        } else if (platform.equals(PLATFORM_IOS))
+        {
+            appiumUrl = "http://10.6.113.26:4723/wd/hub";;
+        }
+        else{
+
+            throw new Exception("Cannot get run platform value from env variable. Platform value " + platform);
+
+        }
+
+        return new URL(appiumUrl);
+    }
+}
